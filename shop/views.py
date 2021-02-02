38a9ffddf2813e5c59228f0ee5fa626dd82969a3
PyGuDev <gubaev1999@gmail.com>
2021-02-03 00:03:15 +0500

    Добавил филтрацию по категориям

--- shop/views.py
+++ shop/views.py
@@ -8,6 +8,20 @@ class MainView(ListView):
     model = Products
     template_name = 'shop/index.html'
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data()
+        context['category'] = CategoryProduct.objects.all()
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get('s')
+        if query:
+            # сортировка по категория
+            queryset = queryset.filter(category=query)
+            print(queryset)
+        return queryset
+
 
 class SingleProductView(DetailView):
     model = Products
